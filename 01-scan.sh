a4ae0e1b12b13803c8e03ca17dec5ccbc0a9abe7
brachiel <brachiel@github.invalid>
2017-08-14 20:22:31 +0200

    Configurable resolution

--- 01-scan.sh
+++ 01-scan.sh
@@ -5,7 +5,7 @@ source ./config.sh
 mkdir -p $RAW_SCAN_DIR
 cd $RAW_SCAN_DIR
 
-scanimage --batch="`date +%Y%m%d_%H%M%S`_%d$SCAN_EXT"  --format=pnm --resolution 300 --mode Color --source ADF
+scanimage --batch="`date +%Y%m%d_%H%M%S`_%d$SCAN_EXT"  --format=pnm --resolution $SCAN_RESOLUTION --mode Color --source ADF
 
 cd ..
 
--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RAW_SCAN_DIR=raw_scans
+SCAN_RESOLUTION=200
 OCR_DIR=ocr_scans
 RAW_SCAN_EXT=.out
 SCAN_EXT=.pnm
